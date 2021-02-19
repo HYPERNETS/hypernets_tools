a731859c317f07c676dd100b0e509354cc7ac7b8
corizzi <alexandre.corizzi@gmail.com>
2021-02-19 16:17:41 +0100

    bad pip command

--- install/01_dependencies.sh
+++ install/01_dependencies.sh
@@ -4,9 +4,9 @@ set -o nounset
 set -euo pipefail
 
 sudo pacman -Sy python-pip tk
-pip uninstall serial
-pip install crcmod pyftdi yoctopuce pyserial
-pip install matplotlib
+python -m pip uninstall serial
+python -m pip install crcmod pyftdi yoctopuce pyserial
+python -m pip install matplotlib
 # Get Access to  /dev/ttySx without 'sudo'
 sudo usermod -a -G uucp $USER
 # Ensure relogin
