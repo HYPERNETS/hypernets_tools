17d4019348208115b97eed2b1a07c04acf68cc45
corizzi <alexandre.corizzi@gmail.com>
2021-02-22 15:07:45 +0100

    del boost inst

--- install/02_install_boost.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-pacman -Sy gcc
-wget https://dl.bintray.com/boostorg/release/1.71.0/source/boost_1_71_0.zip 
-unzip boost_1_71_0.zip
-cd boost_1_71_0/
-./bootstrap.sh
-sudo ./b2 install
-# Add the library path to env
-echo 'LD_LIBRARY_PATH="/usr/local/lib"' | sudo tee -a /etc/environment > /dev/null
