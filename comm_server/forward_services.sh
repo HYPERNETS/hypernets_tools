a3f1b7675f61bddd20fed4f7d4a5b9738b8c9b20
corizzi <alexandre.corizzi@gmail.com>
2021-03-26 15:45:22 +0100

    mistake on credent

--- comm_server/forward_services.sh
+++ comm_server/forward_services.sh
@@ -8,6 +8,11 @@ if [[ ${PWD##*/} != "hypernets_tools" ]]; then
 	exit 1
 fi
 
-ssh -g -N -T -o "ServerAliveInterval 10" -o "ExitOnForwardFailure yes" -R4444:10.42.0.184:4444 hypernets@oceane.obs-vlfr.fr
 
-ssh -g -N -T -o ServerAliveInterval 10 -o ExitOnForwardFailure yes -R8888:127.0.0.1:8888 hypernets@oceane.obs-vlfr.fr
+ipServer=$(awk -F "= " '/credentials/ {print $2}' config_hypernets.ini)
+
+ssh -g -N -T -o "ServerAliveInterval 10" -o "ExitOnForwardFailure yes" \
+	-R4444:10.42.0.184:4444 $ipServer
+
+ssh -g -N -T -o ServerAliveInterval 10 -o ExitOnForwardFailure yes \
+	-R8888:127.0.0.1:8888 $ipServer
