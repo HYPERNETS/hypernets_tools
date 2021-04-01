2dd1d51edb503e7d4e73f96c9f305e87e2c198a6
corizzi <alexandre.corizzi@gmail.com>
2021-04-01 09:56:46 +0100

    jupyter install should be done as normal user

--- install/03_install_jupyter.sh
+++ install/03_install_jupyter.sh
@@ -3,6 +3,15 @@
 set -o nounset
 set -euo pipefail
 
+if [[ $EUID -eq 0 ]]; then
+	echo "This script should not be run as root, use $0 (whithout sudo) instead" 1>&2
+	read -p "   Continue anyway (y/n) ?" -rn1
+	if [[ ! $REPLY =~ ^[Yy]$ ]]; then 
+		echo
+		exit 1
+	fi
+fi
+
 
 python -m pip install jupyter
 
@@ -17,10 +26,8 @@ sed -i "s/# c.NotebookApp.ip = 'localhost'/c.NotebookApp.ip = '0.0.0.0'/g"\
 sed -i "s/# c.NotebookApp.allow_origin = ''/c.NotebookApp.allow_origin = '*'/g"\
 	~/.jupyter/jupyter_notebook_config.py
 
-
 # Then, try :
 # jupyter notebook --no-browser
-
 
 python -m pip install voila
 # Try : 
