ace3b2a371a5c5c7425465c4d9f57e79bdd9066c
Javier A. Concha <jaconcha@gmail.com>
2021-06-01 16:22:32 +0200

    journalctl reverted to -n option

--- comm_server/hello_server.sh
+++ comm_server/hello_server.sh
@@ -42,8 +42,8 @@ shopt -u extglob
 
 # Make Logs
 mkdir -p LOGS
-journalctl -eu hypernets-sequence --since="2days ago" --no-pager > LOGS/hypernets-sequence.log
-journalctl -eu hypernets-hello --since="2days ago" --no-pager > LOGS/hypernets-hello.log
+journalctl -eu hypernets-sequence -n 1500 --no-pager > LOGS/hypernets-sequence.log
+journalctl -eu hypernets-hello -n 150 --no-pager > LOGS/hypernets-hello.log
 
 # Update the datetime flag on the server
 echo "Touching $ipServer:$remoteDir/system_is_up"
