522aba4fa8c6983643e3f7d4edbe481e9599fa24
corizzi <alexandre.corizzi@gmail.com>
2021-10-08 08:00:34 +0000

    enable update yoctohub service

--- install/00_install_yoctohub.sh
+++ install/00_install_yoctohub.sh
@@ -43,15 +43,19 @@ last_virtualhub="VirtualHub.linux.$VERSION.zip"
 virtualhub_link="http://www.yoctopuce.com/FR/downloads/$last_virtualhub"
 
 cd /tmp
+rm -rf Yoctopuce "$last_virtualhub" # In case of previous error
 wget "$virtualhub_link" # Script will stop here if E404 is raised
 
 mkdir Yoctopuce
 unzip "$last_virtualhub" -d Yoctopuce/
 
+systemctl stop yvirtualhub.service
+systemctl disable yvirtualhub.service
+
 # udev rules :
 sudo cp Yoctopuce/udev_conf/51-yoctopuce_all.rules /etc/udev/rules.d/
 
-# - 1: copy VitualHub binary to /usr/sbin
+# - 1: copy VirtualHub binary to /usr/sbin
 cp Yoctopuce/64bits/VirtualHub /usr/sbin
 
 # - 2: ensure that the /usr/sbin/Virtualhub
@@ -66,7 +70,10 @@ rm -rf Yoctopuce "$last_virtualhub"
 
 echo
 echo "---------------------------------------------------------------"
-echo "Try to run the VirtualHub with /usr/sbin/VirtualHub"
-echo "and go to this webpage : 10.42.0.1:4444"
+# echo "Try to run the VirtualHub with /usr/sbin/VirtualHub"
+# echo "Try to go to this webpage : :4444"
+
+midori -e win-new -e goto "http://localhost:4444" &
+journalctl -eu yvirtualhub --follow
 
 # Note previous used version of VirtualHub : 40924
