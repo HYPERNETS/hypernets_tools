e2946a37279b4b7ca20b19a78a5134da9a98d3a5
corizzi <alexandre.corizzi@gmail.com>
2023-02-15 15:09:36 +0000

    update yocto script

--- install/00_install_yoctohub.sh
+++ install/00_install_yoctohub.sh
@@ -18,64 +18,20 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 VERBOSE=0
-VERSION=0
 
 if [ -z "${1-0}" ] ; then usage ; fi
 while getopts 'hvn:' OPTION; do
 	case "$OPTION" in
 		v) VERBOSE=1;;
-		n) VERSION="$OPTARG" ;;
 		?|h) usage ;;
 	esac
 done
 
 if [ "$VERBOSE" -eq 1 ] ; then
 	echo "(Verbose Mode On)" 
-	echo "VERSION provided : $VERSION" 
 fi
 
-if [ "$VERSION" -eq 0 ] ; then 
-	output=$(wget https://www.yoctopuce.com/FR/common/getLastFirmwareLink.php -O -)
-	VERSION=$(echo $output | cut -d',' -f1 | cut -d':' -f2)
-	echo "Last version found on www.yoctopuce.com : $VERSION"
-fi
-last_virtualhub="VirtualHub.linux.$VERSION.zip"
-virtualhub_link="http://www.yoctopuce.com/FR/downloads/$last_virtualhub"
-
-cd /tmp
-rm -rf Yoctopuce "$last_virtualhub" # In case of previous error
-wget "$virtualhub_link" # Script will stop here if E404 is raised
-
-mkdir Yoctopuce
-unzip "$last_virtualhub" -d Yoctopuce/
-
-set +e
-systemctl stop yvirtualhub.service
-systemctl disable yvirtualhub.service
-set -e
-
-# udev rules :
-sudo cp Yoctopuce/udev_conf/51-yoctopuce_all.rules /etc/udev/rules.d/
-
-# - 1: copy VirtualHub binary to /usr/sbin
-cp Yoctopuce/64bits/VirtualHub /usr/sbin
-
-# - 2: ensure that the /usr/sbin/Virtualhub
-chmod +x /usr/sbin/VirtualHub
-
-cp -f Yoctopuce/startup_script/yvirtualhub.service /etc/systemd/system/
-systemctl daemon-reload
-systemctl start yvirtualhub.service
-systemctl enable yvirtualhub.service
-
-rm -rf Yoctopuce "$last_virtualhub"
-cd -
-echo "Installation  VirtualHub done."
-echo "---------------------------------------------------------------"
-
-# echo "Try to run the VirtualHub with /usr/sbin/VirtualHub"
-# echo "and go to this webpage"
-# midori -e win-new -e goto "http://localhost:4444" &
-# journalctl -eu yvirtualhub --follow
-
-# Note previous used version of VirtualHub : 40924
+wget -qO - https://www.yoctopuce.com/apt/KEY.gpg |  sudo apt-key add -
+echo "deb https://www.yoctopuce.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/yoctopuce.list 
+sudo apt update
+sudo apt install virtualhub
