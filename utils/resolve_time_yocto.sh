e499680794a136ffdb0352ce15f7a8d9aeea2138
corizzi <alexandre.corizzi@gmail.com>
2023-03-10 15:27:44 +0000

    time resol

---
+++ utils/resolve_time_yocto.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/bash
+
+
+systemctl status yvirtualhub --no-pager
+# sleep 15
+echo "Date/Time of the computer:"
+date +"%Y/%m/%d %H:%M:%S%z"
+
+echo "Getting Yocto-Pictor-WiFi's Date/Time"
+# TODO : yocto id
+YoctoRTC=$(wget -O- http://127.0.0.1:4444/bySerial/OBSVLFR2-13F9AE/api/realTimeClock/dateTime)
+err=$?
+
+if [ $err -eq 0 ] ; then
+	echo "Yocto RTC : $YoctoRTC"
+	echo "Mise à jour de l'heure du PC"
+	sudo date -s "$YoctoRTC"  +"%Y/%m/%d %H:%M:%S%z"
+	echo "Heure corrigée du PC"
+	date +"%Y/%m/%d %H:%M:%S%z"
+else
+	echo "Error: fail to get YoctoRTC (wget outputs $err)"
+fi
