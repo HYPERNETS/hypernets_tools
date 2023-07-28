15d12ac0757fe0a412f349893ef0adbead5d8f58
Joel Kuusk <joelk@ut.ee>
2023-07-28 15:41:03 +0000

    Configparser exits with error if config file is missing

--- utils/configparser.sh
+++ utils/configparser.sh
@@ -16,12 +16,20 @@
 set -o nounset                              # Treat unset variables as an error
 set -euo pipefail                           # Bash Strict Mode
 
+## define text highlights
+XHL=$(tput setaf 9) ## red
+RESET_HL=$(tput sgr0) ## reset all text formatting
 
 parse_config () {
 	keyword="$1"
 	config_file="$2"
-	value=$(awk -F "[=]" '/^'$keyword'/ {
-	print $2; exit}' $config_file)
+
+	if [[ ! -f "$config_file" ]]; then
+		>&2 echo "${XHL}Config file $config_file not found${RESET_HL}"
+		exit -1
+	fi
+	
+	value=$(awk -F "[=]" '/^'$keyword'/ {print $2; exit}' $config_file)
 	value=$(echo "$value" | tr -d ' ')
 	echo $value
 }
