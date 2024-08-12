704a18587064e331c3be8ff49dbb98b0cde85211
Joel Kuusk <joelk@ut.ee>
2024-08-12 14:50:12 +0000

    Use loglevel from config when waiting for instrument to boot

--- hypernets/hypstar/handler.py
+++ hypernets/hypstar/handler.py
@@ -36,7 +36,9 @@ class HypstarHandler(Hypstar):
             error("Failed to get instrument instance")
             exit(40)
 
-        if expect_boot_packet and not wait_for_instrument(instrument_port, boot_timeout): # noqa
+        if expect_boot_packet and not wait_for_instrument(port=instrument_port, 
+                                                          timeout_s=boot_timeout, 
+                                                          loglevel=instrument_loglevel):
             # just in case instrument sent BOOTED packet while we were
             # switching baudrates, let's test if it's there
             try:
